dcd5fe69d714f95f1e3dc5972eca55dc64fbc50f
unknown <pabloisrael212006@gmail.com>
2023-08-15 15:42:59 -0300

    Adicionada tarefa do modulo 8

---
+++ Tarefa_Modulo8/src/NotasDosAlunos.java
@@ -0,0 +1,17 @@
+public class NotasDosAlunos {
+    public static void main(String[] args) {
+        // Teste do comando para imprimir no console.
+        System.out.println(":D");
+
+        int aluno1 = 9;
+        int aluno2 = 7;
+        int aluno3 = 3;
+        int aluno4 = 7;
+
+        int somaDasNotas = aluno1 + aluno2 + aluno3 + aluno4;
+
+        int mediaDasNotas = somaDasNotas / 4;
+
+        System.out.println(mediaDasNotas);
+    }
+}
