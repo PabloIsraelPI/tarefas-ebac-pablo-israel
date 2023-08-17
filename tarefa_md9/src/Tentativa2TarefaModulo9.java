32d6b15b078075cd6120aa763362c09bc264c854
unknown <pabloisrael212006@gmail.com>
2023-08-17 17:10:49 -0300

    Corrigida a tarefa do md9

---
+++ tarefa_md9/src/Tentativa2TarefaModulo9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Tentativa2TarefaModulo9 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Insira o valor a srguir:");
+
+        int valor = scanner.nextInt();
+
+        System.out.println("Voçê digitou:" + valor);
+//A seguir é feito o casting.
+        System.out.println("[Variavel primitiva]" + valor);
+
+        Long valor2 = (long) valor;
+        System.out.println("[Wrapper]" + valor2);
+
+
+    }
+}
