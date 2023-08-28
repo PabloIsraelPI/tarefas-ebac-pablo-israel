884f2fa6a1f4d415b72f45d7afede22e25b80ef8
unknown <pabloisrael212006@gmail.com>
2023-08-28 16:41:21 -0300

    correção tarefa md13

--- tarefa_md13/src/Pessoa.java
+++ tarefa_md13/src/Pessoa.java
@@ -3,32 +3,5 @@ public class Pessoa {
         System.out.println("nome Alan");
     }
 
-    public class Pessoajuridica extends Pessoa {
-
-
-        @Override
-        void nome() {
-            super.nome();
-        }
-
-        void EmpresadeComercio() {
-            System.out.println("Vende Produtos.");
-        }
-
-    }
-
-    public class PessoaFisica extends Pessoa {
-
-
-        @Override
-        void nome() {
-            super.nome();
-        }
-
-        void Cidadãolegal() {
-            System.out.println("Tem seus direitos.");
-        }
-    }
-
 
 }
---
+++ tarefa_md13/src/Pessoajuridica.java
@@ -0,0 +1,9 @@
+class Pessoajuridica extends Pessoa {
+    @Override
+    void nome() {
+        super.nome();
+    }
+    void Empresa(){
+    System.out.println("vende produtos");
+    }
+}
