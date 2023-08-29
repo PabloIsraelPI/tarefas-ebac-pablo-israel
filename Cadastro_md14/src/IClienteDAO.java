a05e8de855d4541e314e58323c10a1461b4337a4
unknown <pabloisrael212006@gmail.com>
2023-08-29 13:59:34 -0300

    adc tarefa md14

---
+++ Cadastro_md14/src/IClienteDAO.java
@@ -0,0 +1,18 @@
+
+import java.util.Collection;
+
+/**
+ * @author rodrigo.pires
+ */
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
