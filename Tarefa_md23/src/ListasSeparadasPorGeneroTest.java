5f9f12b13b30d2f0fd588556c6f3ccaedc444636
unknown <pabloisrael212006@gmail.com>
2023-09-26 17:04:50 -0300

    adcionada a tarefa do md23 incompleta

---
+++ Tarefa_md23/src/ListasSeparadasPorGeneroTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class ListasSeparadasPorGeneroTest {
+    public void testeDaSegundaLista(){
+        List<String> listaFeminina;
+        listaFeminina = ListasSeparadasPorGenero.getNomesFemininos();
+
+    }
+
+}
